78b7873a170bb3be45e9960467a3465b6152646e
anilamohanan <anilamohanan89@gmail.com>
2026-07-07 21:55:56 +0530

    Add Review API

--- Server/server.js
+++ Server/server.js
@@ -6,10 +6,12 @@ const redisClient = require("./config/redis");
 const productRoutes = require("./routes/productRoutes");
 const mongoose = require("mongoose");
 //const authRoutes = require("./routes/authRoutes");
+
 const categoryRoutes = require("./routes/categoryRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const wishlistRoutes = require("./routes/wishlistRoutes");
 const userRoutes = require("./routes/userRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 app.use(express.json());
@@ -20,6 +22,7 @@ app.use("/orders", orderRoutes);
 app.use("/wishlist",wishlistRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
+app.use("/reviews", reviewRoutes);
 
 connectDB();
 
